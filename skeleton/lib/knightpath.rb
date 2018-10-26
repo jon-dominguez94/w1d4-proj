261141644fb994c49a0f078d9a916782db57d5e9
jon-dominguez94 <jon.dominguez94@yahoo.com>
2018-10-26 14:43:47 -0700

    Create KnightPathFinder class with #initialize, #valid_moves, #new_move_positions

---
+++ skeleton/lib/knightpath.rb
@@ -0,0 +1,52 @@
+require_relative '00_tree_node'
+require 'byebug'
+
+class KnightPathFinder
+  
+  def initialize(pos)
+    @root_node = PolyTreeNode.new(pos)
+    build_move_tree
+    @visited_positions = [pos]
+  end
+  
+  def build_move_tree
+    
+  end
+  
+  def valid_moves(pos)
+    all_moves = []
+    row, col = pos
+    all_moves += get_north(pos) + get_south(pos) + get_west(pos) + get_east(pos)
+    all_moves.select {|pos| pos.all? {|el| el.between?(0,7)}} 
+  end
+  
+  def new_move_positions(pos)
+    # debugger
+    potential_moves = valid_moves(pos)
+    potential_moves -= @visited_positions
+    @visited_positions += potential_moves
+    potential_moves
+  end
+  
+  private
+  
+  def get_north(pos)
+    row, col = pos
+    [[row - 3, col + 1], [row - 3, col - 1]]
+  end
+  
+  def get_south(pos)
+    row, col = pos
+    [[row + 3, col + 1], [row + 3, col - 1]]
+  end
+  
+  def get_east(pos)
+    row, col = pos
+    [[row - 1, col + 3], [row + 1, col + 3]]
+  end
+  
+  def get_west(pos)
+    row, col = pos
+    [[row - 1, col - 3], [row + 1, col - 3]]
+  end
+end
